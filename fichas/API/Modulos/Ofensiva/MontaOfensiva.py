52a9429e2fa7faf2aef72f7a44692b6572617562
Camila Silva <88329483+camilacrsilva@users.noreply.github.com>
2023-12-03 15:30:19 -0300

    Add files via upload

---
+++ fichas/API/Modulos/Ofensiva/MontaOfensiva.py
@@ -0,0 +1,4 @@
+# a Ofensiva precisa de estrátegia 
+# a iniciativa vem de Poderes + Vantagens + Habilidades
+# O Acerto é Pericia + Vantagem + Habilidade
+# e O EFeito ou Dano é Habilidade ou Vangem ou Poder
---
+++ fichas/API/Modulos/__init__.py
@@ -0,0 +1,3 @@
+# from .Pericias import PericiaClass as Pericia
+# from .Poderes import BibliotecaDeClasses as Efeito
+# from .Instanciadora import instanciadora
